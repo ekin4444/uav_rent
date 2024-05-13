6edd5c32831731e2d08c62c5ca9d1396da69833f
ekinf <ekinfiliza@hotmail.com>
2024-05-13 18:42:50 +0200

    feat: base added

--- uav_rent/urls.py
+++ uav_rent/urls.py
@@ -19,8 +19,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from core import views
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
