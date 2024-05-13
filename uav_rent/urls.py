91a62d5838e5b4be4e55925126d78ecb5a576f4a
ekinf <ekinfiliza@hotmail.com>
2024-05-13 15:19:43 +0200

    feat: template added

--- uav_rent/urls.py
+++ uav_rent/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for uav_rent project.
+URL configuration for iha_rental_project2 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('core.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
